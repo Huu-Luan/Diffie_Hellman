ae8d670f2803e340cd3f5fd4d6572da9c5c15c5e
Huu-Luan <145535660+Huu-Luan@users.noreply.github.com>
2024-11-11 18:12:30 +0700

    Add files via upload

---
+++ Diffie Hellman/Client/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Net.Sockets;
+using System.Security.Cryptography;
+using System.Text;
+
+class Client
+{
+    private static readonly string serverIp = "127.0.0.1";
+    private static readonly int port = 8080;
+    private static readonly ECDiffieHellmanCng diffieHellman = new ECDiffieHellmanCng();
+
+    static void Main(string[] args)
+    {
+        diffieHellman.KeySize = 256;
+        byte[] clientPublicKey = diffieHellman.PublicKey.ToByteArray();
+
+        using (TcpClient client = new TcpClient(serverIp, port))
+        using (NetworkStream stream = client.GetStream())
+        {
+            Console.WriteLine("Connected to server.");
+
+            byte[] serverPublicKey = new byte[72]; 
+            stream.Read(serverPublicKey, 0, serverPublicKey.Length);
+            Console.WriteLine("Received public key from server.");
+
+            stream.Write(clientPublicKey, 0, clientPublicKey.Length);
+            Console.WriteLine("Sent public key to server.");
+
+            var serverKey = CngKey.Import(serverPublicKey, CngKeyBlobFormat.EccPublicBlob);
+            byte[] sharedSecret = diffieHellman.DeriveKeyMaterial(serverKey);
+            Console.WriteLine("Shared secret key generated.");
+
+            Console.WriteLine("Shared Secret Key: " + Convert.ToBase64String(sharedSecret));
+
+            byte[] encryptedMessage = new byte[256]; 
+            stream.Read(encryptedMessage, 0, encryptedMessage.Length);
+            string decryptedMessage = DecryptMessage(encryptedMessage, sharedSecret);
+            Console.WriteLine("Decrypted message from server: " + decryptedMessage);
+        }
+    }
+
+    private static string DecryptMessage(byte[] encryptedMessage, byte[] key)
+    {
+        using (Aes aes = Aes.Create())
+        {
+            aes.Key = key;
+
+            byte[] iv = new byte[aes.BlockSize / 8];
+            Array.Copy(encryptedMessage, iv, iv.Length);
+            aes.IV = iv;
+
+            using (var decryptor = aes.CreateDecryptor(aes.Key, aes.IV))
+            {
+                byte[] decryptedBytes = new byte[encryptedMessage.Length - iv.Length];
+                decryptor.TransformBlock(encryptedMessage, iv.Length, decryptedBytes.Length, decryptedBytes, 0);
+                return Encoding.UTF8.GetString(decryptedBytes).TrimEnd('\0');
+            }
+        }
+    }
+}
---
+++ Diffie Hellman/Server/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Security.Cryptography;
+using System.Text;
+
+class Server
+{
+    private static readonly int port = 8080;
+    private static readonly ECDiffieHellmanCng diffieHellman = new ECDiffieHellmanCng();
+
+    static void Main(string[] args)
+    {
+        diffieHellman.KeySize = 256;
+        byte[] serverPublicKey = diffieHellman.PublicKey.ToByteArray();
+
+        TcpListener server = new TcpListener(IPAddress.Any, port);
+        server.Start();
+        Console.WriteLine("Server started. Waiting for a connection...");
+
+        using (TcpClient client = server.AcceptTcpClient())
+        using (NetworkStream stream = client.GetStream())
+        {
+            Console.WriteLine("Client connected.");
+            stream.Write(serverPublicKey, 0, serverPublicKey.Length);
+            Console.WriteLine("Sent public key to client.");
+
+            byte[] clientPublicKey = new byte[72];
+            stream.Read(clientPublicKey, 0, clientPublicKey.Length);
+            Console.WriteLine("Received public key from client.");
+
+            var clientKey = CngKey.Import(clientPublicKey, CngKeyBlobFormat.EccPublicBlob);
+            byte[] sharedSecret = diffieHellman.DeriveKeyMaterial(clientKey);
+            Console.WriteLine("Shared secret key generated.");
+            Console.WriteLine("Shared Secret Key: " + Convert.ToBase64String(sharedSecret));
+
+            string message = "Hello from Luan!";
+            byte[] encryptedMessage = EncryptMessage(message, sharedSecret);
+            stream.Write(encryptedMessage, 0, encryptedMessage.Length);
+            Console.WriteLine("Encrypted message sent to client.");
+        }
+        server.Stop();
+    }
+
+    private static byte[] EncryptMessage(string message, byte[] key)
+    {
+        using (Aes aes = Aes.Create())
+        {
+            aes.Key = key;
+            aes.GenerateIV();
+            using (var encryptor = aes.CreateEncryptor(aes.Key, aes.IV))
+            {
+                byte[] messageBytes = Encoding.UTF8.GetBytes(message);
+                byte[] encryptedMessage = new byte[aes.IV.Length + messageBytes.Length];
+
+                Array.Copy(aes.IV, 0, encryptedMessage, 0, aes.IV.Length);
+                encryptor.TransformBlock(messageBytes, 0, messageBytes.Length, encryptedMessage, aes.IV.Length);
+
+                return encryptedMessage;
+            }
+        }
+    }
+}
